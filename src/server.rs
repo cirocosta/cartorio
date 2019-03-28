8849a1f95e3aef4820d2fd7e3f1bf0ce17eb47e7
Ciro S. Costa <cscosta@pivotal.io>
2019-03-28 08:39:14 -0400

    add initial http server impl
    Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>

--- src/server.rs
+++ src/server.rs
@@ -1,25 +1,53 @@
 extern crate hyper;
 
-use hyper::rt::Future;
 use hyper::service::service_fn_ok;
-use hyper::{Body, Request, Response, Server};
+use hyper::rt::Future;
+use hyper::{Body, Request, Response, Server, Method, StatusCode};
 use std::net::SocketAddr;
 
-const PHRASE: &str = "Hello, World!";
+const BODY_NOT_FOUND: &str = "not found";
 
-fn handle_hello_world(_req: Request<Body>) -> Response<Body> {
-    Response::new(Body::from(PHRASE))
+/// Handles incoming requests and dispatches them to the
+/// appropriate function that is supposed to handle them.
+///
+fn handle_routing(req: Request<Body>) -> Response<Body> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/_live") => {
+            let body = Body::from("alive");
+
+           Response::builder()
+                         .status(StatusCode::OK)
+                         .body(body)
+                         .unwrap()
+        },
+
+        (&Method::GET, "/v2") => {
+            let body = Body::from("alive");
+
+           Response::builder()
+                         .status(StatusCode::OK)
+                         .header("Docker-Distribution-Api-Version", "registry/2.0")
+                         .body(body)
+                         .unwrap()
+        },
+
+        _ => {
+           Response::builder()
+                         .status(StatusCode::NOT_FOUND)
+                         .body(Body::from(BODY_NOT_FOUND))
+                         .unwrap()
+        }
+    }
 }
 
+/// Starts an HTTP server at an address specified as `address`.
+///
 pub fn serve(address: &str) {
     let addr: SocketAddr = address.parse().unwrap();
-    let new_svc = || service_fn_ok(handle_hello_world);
-
     let server = Server::bind(&addr)
-        .serve(new_svc)
+        .serve( || service_fn_ok(handle_routing))
         .map_err(|e| println!("server error: {}", e));
 
     println!("listening on {}", address);
-
     hyper::rt::run(server);
 }
