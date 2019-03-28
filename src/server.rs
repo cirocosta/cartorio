a1152751ec40e7ee7960acd8ed93da0f60ddc5ce
Ubuntu <ubuntu@bionic>
2019-03-28 12:45:51 +0000

    simplify server
    Signed-off-by: Ubuntu <ubuntu@bionic>

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,8 @@
 extern crate hyper;
 
-use hyper::service::service_fn_ok;
 use hyper::rt::Future;
-use hyper::{Body, Request, Response, Server, Method, StatusCode};
+use hyper::service::service_fn_ok;
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use std::net::SocketAddr;
 
 const BODY_NOT_FOUND: &str = "not found";
@@ -12,31 +12,21 @@ const BODY_NOT_FOUND: &str = "not found";
 ///
 fn handle_routing(req: Request<Body>) -> Response<Body> {
     match (req.method(), req.uri().path()) {
-        (&Method::GET, "/_live") => {
-            let body = Body::from("alive");
+        (&Method::GET, "/_live") => Response::builder()
+            .status(StatusCode::OK)
+            .body(Body::from("alive"))
+            .unwrap(),
 
-           Response::builder()
-                         .status(StatusCode::OK)
-                         .body(body)
-                         .unwrap()
-        },
+        (&Method::GET, "/v2") => Response::builder()
+            .status(StatusCode::OK)
+            .header("docker-distribution-api-version", "registry/2.0")
+            .body(Body::empty())
+            .unwrap(),
 
-        (&Method::GET, "/v2") => {
-            let body = Body::from("alive");
-
-           Response::builder()
-                         .status(StatusCode::OK)
-                         .header("Docker-Distribution-Api-Version", "registry/2.0")
-                         .body(body)
-                         .unwrap()
-        },
-
-        _ => {
-           Response::builder()
-                         .status(StatusCode::NOT_FOUND)
-                         .body(Body::from(BODY_NOT_FOUND))
-                         .unwrap()
-        }
+        _ => Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::from(BODY_NOT_FOUND))
+            .unwrap(),
     }
 }
 
@@ -45,7 +35,7 @@ fn handle_routing(req: Request<Body>) -> Response<Body> {
 pub fn serve(address: &str) {
     let addr: SocketAddr = address.parse().unwrap();
     let server = Server::bind(&addr)
-        .serve( || service_fn_ok(handle_routing))
+        .serve(|| service_fn_ok(handle_routing))
         .map_err(|e| println!("server error: {}", e));
 
     println!("listening on {}", address);
